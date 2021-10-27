4fcd495579a530cfd21d912871efec1b58bf1745
ViriatoNova <viriatosampaio@gmail.com>
2021-10-27 11:23:58 -0300

    edit cardapio

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 /**
- * Função navegação das paginas
+ * Função navegação das paginas desk
  * 
  */
  (function (){
@@ -9,6 +9,49 @@
     const btnHome = document.querySelector('#btn-home');
     const btnCardapio = document.querySelector('#btn-cardapio');
     const btnHistoria = document.querySelector('#btn-historia');
+
+    const pages = [home, cardapio, historia] 
+        
+    btnHome.addEventListener('click', function() {
+        
+        for (const el of pages) {  
+            if (el.classList.contains('hidden') === false) { el.classList.toggle('hidden') }
+        };
+        
+        home.classList.toggle('hidden');
+    });
+    
+    btnCardapio.addEventListener('click', function() {
+        
+        for (const el of pages) {  
+            if (el.classList.contains('hidden') === false) { el.classList.toggle('hidden') }
+        };
+        
+        cardapio.classList.toggle('hidden');
+    });
+    
+    btnHistoria.addEventListener('click', function() {
+        
+        for (const el of pages) {  
+            if (el.classList.contains('hidden') === false) { el.classList.toggle('hidden') }
+        };
+        
+        historia.classList.toggle('hidden');
+    }); 
+
+})();
+
+/**
+ * Função navegação das paginas mobile
+ * 
+ */
+ (function (){
+    const home = document.querySelector('#home');
+    const cardapio = document.querySelector('#cardapio');
+    const historia = document.querySelector('#historia');
+    const btnHome = document.querySelector('#btn-mobhome');
+    const btnCardapio = document.querySelector('#btn-mobcardapio');
+    const btnHistoria = document.querySelector('#btn-mobhistoria');
 
     const pages = [home, cardapio, historia] 
         
